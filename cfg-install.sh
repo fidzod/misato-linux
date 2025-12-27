f1e1df4d9197a260bbb02aa0cc5c36f144cddb1e
Fidzod <zodshuk@posteo.de>
2025-12-27 23:17:41 +0000

    Added a TUI configurator to the installer with gum.

---
+++ cfg-install.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+declare ST_KEYBOARD="Keyboard"
+declare ST_HOSTNAME="Hostname"
+declare ST_TIME="Timezone"
+declare ST_DISKS="Disk Partitioning and Filesystems"
+declare ST_PASSWD="Root Password"
+declare ST_USER="User Account"
+declare ST_QUIT="Install"
+
+declare -A CONFIG_STATUS=(
+    ["$ST_KEYBOARD"]=
+    ["$ST_HOSTNAME"]=
+    ["$ST_TIME"]=
+    ["$ST_DISKS"]=
+    ["$ST_PASSWD"]=
+    ["$ST_USER"]=
+    ["$ST_QUIT"]=
+)
+
+declare QUIT=
+
+declare CF_KEYBOARD=
+declare CF_HOSTNAME=
+declare CF_TIME=
+declare CF_DISKS=
+declare CF_PASSWD=
+declare CF_USERNAME=
+declare CF_USERPASSWD=
+
+check_progress () { [[ ${CONFIG_STATUS["$1"]} ]] && echo "$1" || echo "*$1"; }
+
+st_keyboard () {
+     CF_KEYBOARD=$(gum choose \
+        --header "Choose keyboard layout: " \
+        --height=7 \
+        --cursor.background="#b78db7" \
+        --cursor.foreground="#d3d3d3" \
+        --header.foreground="#b78db7" \
+        --item.foreground="#d3d3d3" \
+        $(localectl list-keymaps)
+    )
+
+    CONFIG_STATUS["$ST_KEYBOARD"]=true
+}
+
+st_hostname () {
+    CF_HOSTNAME=$(gum input \
+        --prompt="Hostname: " \
+        --prompt.foreground="#b78db7" \
+        --value="misato"
+    )
+
+    CONFIG_STATUS["$ST_HOSTNAME"]=true
+}
+
+st_disks () {
+    CF_DISKS=$(gum choose \
+        --header "Disk Partioning" \
+        --height=7 \
+        --cursor.background="#b78db7" \
+        --cursor.foreground="#d3d3d3" \
+        --header.foreground="#b78db7" \
+        --item.foreground="#d3d3d3" \
+        "Automatic (Best effort)" \
+        "Manual (Bash prompt, cfdisk and fdisk available)"
+    )
+
+    CONFIG_STATUS["$ST_DISKS"]=true
+}
+
+st_password () {
+    CF_PASSWD=$(gum input \
+        --prompt="Root Password: " \
+        --password \
+        --prompt.foreground="#b78db7"
+    )
+
+    CONFIG_STATUS["$ST_PASSWD"]=true
+}
+
+st_time () {
+    CF_TIME=$(gum file \
+        --height=10 \
+        --no-permissions \
+        --no-size \
+        --cursor.foreground="#b78db7" \
+        --selected.foreground="#b78db7" \
+        --directory.foreground="#5a5a97" \
+        --file.foreground="#d3d3d3" \
+        /usr/share/zoneinfo 
+    )
+
+    CONFIG_STATUS["$ST_TIME"]=true
+}
+
+make_user () {
+    CF_USERNAME=$(gum input \
+        --prompt="Account username: " \
+        --prompt.foreground="#b78db7" \
+        --header.foreground="#c5b0c5" \
+        --header="Create a user"
+    )
+    CF_USERPASSWD=$(gum input \
+        --prompt="Account password: " \
+        --prompt.foreground="#b78db7" \
+        --header.foreground="#c5b0c5" \
+        --header="Create a user" \
+        --password
+    )
+    
+    CONFIG_STATUS["$ST_USER"]=true
+}
+
+banner () {
+    BANNER_HEAD=$(gum style \
+        --width=50 \
+        --align center \
+        --foreground="#b78db7" \
+        "Misato Linux"
+    )
+    BANNER_SUBH=$(gum style \
+        --width=50 \
+        --align center \
+        --foreground="#d3d3d3" \
+        "Welcome to the Misato Installer for Void Linux!"
+    )
+
+    gum style \
+        --border rounded \
+        --padding "1 4" \
+        --border-foreground "#c5b0c5" \
+        "$(gum join --vertical "$BANNER_HEAD" "" "$BANNER_SUBH")"
+
+    echo ""
+}
+
+main () {
+    clear
+
+    banner
+
+    OPTION=$(gum choose \
+        --header="Configure Installation:" \
+        --height=7 \
+        --cursor.background="#b78db7" \
+        --cursor.foreground="#d3d3d3" \
+        --header.foreground="#b78db7" \
+        --item.foreground="#d3d3d3" \
+        --label-delimiter=":" \
+        "$(check_progress "$ST_KEYBOARD"):$ST_KEYBOARD" \
+        "$(check_progress "$ST_HOSTNAME"):$ST_HOSTNAME" \
+        "$(check_progress "$ST_TIME"):$ST_TIME" \
+        "$(check_progress "$ST_DISKS"):$ST_DISKS" \
+        "$(check_progress "$ST_PASSWD"):$ST_PASSWD" \
+        "$(check_progress "$ST_USER"):$ST_USER" \
+        "$(check_progress "$ST_QUIT"):$ST_QUIT")
+
+    case $OPTION in
+        "$ST_KEYBOARD") st_keyboard ;;
+        "$ST_HOSTNAME") st_hostname ;;
+        "$ST_TIME") st_time ;;
+        "$ST_DISKS") st_disks ;;
+        "$ST_PASSWD") st_password ;;
+        "$ST_USER") make_user ;;
+        "$ST_QUIT") QUIT=1 ;;
+    esac
+
+    [[ -z "$QUIT" ]] && main
+}
+
+main
+
+gum pager --height=14 --width=60 <<EOF
+# ========================== #
+# Misato Linux Configuration #
+# ========================== #
+
+CF_KEYBOARD=$CF_KEYBOARD
+CF_HOSTNAME=$CF_HOSTNAME
+CF_TIME=$CF_TIME
+CF_DISKS=$CF_DISKS
+CF_PASSWD=$CF_PASSWD
+CF_USERNAME=$CF_USERNAME
+CF_USERPASSWD=$CF_USERPASSWD
+EOF
--- install.sh
+++ install.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+# Todo
+# Use gum log
+
+source ./cfg_install.sh
+
 echo "Misato Installer for Void Linux, V.0.1.0"
 
-echo "Partitioning disk /dev/sda..."
-echo -e "label: gpt\n1G,1G,U\n,,L" | sfdisk /dev/sda
+if [[ $CF_DISKS == "Automatic (Best effort)" ]]; then
+    echo "Using best effort partitioning scheme..."
+    echo -e "label: gpt\n1G,1G,U\n,,L" | sfdisk /dev/sda
 
-echo "Creating filesystems..."
-mkfs.vfat /dev/sda1
-mkfs.ext4 /dev/sda2
-mount /dev/sda2 /mnt
-mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi/
+    echo "Creating filesystems..."
+    mkfs.vfat /dev/sda1
+    mkfs.ext4 /dev/sda2
+    mount /dev/sda2 /mnt
+    mkdir -p /mnt/boot/efi
+    mount /dev/sda1 /mnt/boot/efi/
+else
+    echo "You have selected to partition disks and install filesystems manually."
+    echo "You will now be dropped onto a bash shell."
+    echo "Available tools include: fdisk, cfdisk, mkfs.ext4, mkfs.vfat"
+    echo "When you are done run 'exit'."
+    bash
+fi
 
 echo "Installing base system..."
 REPO=https://repo-default.voidlinux.org/current/musl
@@ -23,14 +36,16 @@ xgenfstab -U /mnt > /mnt/etc/fstab
 
 echo "Entering chroot..."
 xchroot /mnt /bin/bash -c "
-    echo 'misato' > /etc/hostname
-    sed -i '/^#KEYMAP=\"es\"/c\KEYMAP=\"uk\"' /etc/rc.conf
-    ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-    echo -e \"misatolinux\nmisatolinux\" | passwd
+    echo '$CF_HOSTNAME' > /etc/hostname
+    sed -i '/^#KEYMAP=\"es\"/c\KEYMAP=\"$CF_KEYBOARD\"' /etc/rc.conf
+    ln -sf $CF_TIME /etc/localtime
+    echo -e \"$CF_PASSWD\n$CF_PASSWD\" | passwd
     ln -s /etc/sv/dhcpcd /var/service/
     xbps-install -S grub-x86_64-efi
     grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=\"Misato\"
     xbps-reconfigure -fa
+    useradd -m -g wheel $CF_USERNAME
+    echo -e \"$CF_USERPASSWD\n$CF_USERPASSWD\" | passwd
 "
 
 echo "Installation complete!"
